60ff783adab15c62a42f148740ba0dc0e4fdea57
ryougiitachi <ryougiitachi@gmail.com>
2017-05-30 12:24:29 +0800

    Lunar calendar still not complete.

--- time_lunar_calendar.h
+++ time_lunar_calendar.h
@@ -8,7 +8,16 @@
 #ifndef TIME_LUNAR_CALENDAR_H_
 #define TIME_LUNAR_CALENDAR_H_
 
-const int SECONDS_OF_DAY = 24 * 60 * 60;
+//const int SECONDS_OF_DAY = 24 * 60 * 60;
+
+typedef struct _TERM_ELP200082_MOON {
+	int a;
+	int b;
+	int c;
+	int d;
+	int amplitudeAsin;
+	int amplitudeAcos;
+} STERM_ELP200082_MOON, PTERM_ELP200082_MOON;
 
 #endif /* TIME_LUNAR_CALENDAR_H_ */
 
